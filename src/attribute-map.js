e1f6030a6c77aa4ea680bfb866cc0dff95984b63
Michael Malone <michael.malone@drivesoftware.co.nz>
2016-06-13 13:57:54 +1200

    fix(attribute mapping): textarea maxlength binding
    allow binding to maxlength attribute on textarea tags
    
    closes #90

--- src/attribute-map.js
+++ src/attribute-map.js
@@ -28,6 +28,8 @@ export class AttributeMap {
     this.register('input', 'formnovalidate', 'formNoValidate');
     this.register('input', 'formtarget', 'formTarget');
 
+    this.register('textarea', 'maxlength', 'maxLength');
+
     this.register('td', 'rowspan', 'rowSpan');
     this.register('td', 'colspan', 'colSpan');
     this.register('th', 'rowspan', 'rowSpan');
