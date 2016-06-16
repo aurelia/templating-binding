b13c42050e0a73dd7b1eabf007d65b5d5fa1ab2f
Rob Eisenberg <rob@bluespire.com>
2016-06-15 22:51:03 -0400

    chore(config): deps update

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ System.config({
   },
 
   map: {
-    "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.2.0.5",
+    "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.2.0.6",
     "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.2.1.0",
     "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.2.0.0",
     "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.3.0",
@@ -49,7 +49,7 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
-    "npm:aurelia-binding@1.0.0-beta.2.0.5": {
+    "npm:aurelia-binding@1.0.0-beta.2.0.6": {
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.2.0.0",
       "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.2.0.0",
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.3.0",
@@ -76,7 +76,7 @@ System.config({
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.3.0"
     },
     "npm:aurelia-templating@1.0.0-beta.3.0.2": {
-      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.2.0.5",
+      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.2.0.6",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.2.1.0",
       "aurelia-loader": "npm:aurelia-loader@1.0.0-beta.2.0.0",
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.2.0.0",
