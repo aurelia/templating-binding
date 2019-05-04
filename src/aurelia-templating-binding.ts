102d1f54f8974188050cc7afc7f121b86f4a3471
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-05-04 15:44:33 +1000

    chore(build): fix build scripts

--- src/aurelia-templating-binding.ts
+++ src/aurelia-templating-binding.ts
@@ -6,3 +6,11 @@ export function configure(config: any) {
   container.registerSingleton(BindingLanguage, TemplatingBindingLanguage);
   container.registerAlias(BindingLanguage, TemplatingBindingLanguage);
 }
+
+export { AttributeMap } from './attribute-map';
+export { TemplatingBindingLanguage } from './binding-language';
+export { SyntaxInterpreter } from './syntax-interpreter';
+export { InterpolationBindingExpression, InterpolationBinding, ChildInterpolationBinding } from './interpolation-binding-expression';
+export { LetExpression, LetBinding } from './let-expression';
+export { LetInterpolationBindingExpression, LetInterpolationBinding } from './let-interpolation-expression';
+export { IAttributeInfo } from './interfaces';
